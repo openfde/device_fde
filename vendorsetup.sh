891bc079b5b61f7b0d858bb0e184199d820a70d0
gongyong <185457686@qq.com>
2024-01-04 10:06:52 +0800

    add systemui into predownload list

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,8 @@
 
 device_fde="device/openfde/fde/fde_vnc"
 function syncFdeApk {
- download_url=`cat ${ANDROID_BUILD_TOP}/${device_fde}/FdeVncClient`
- wget ${download_url} -O ${ANDROID_BUILD_TOP}/${device_fde}/FdeVncClient.apk
+ vnc_download_url=`cat ${ANDROID_BUILD_TOP}/${device_fde}/FdeVncClient`
+ wget ${vnc_download_url} -O ${ANDROID_BUILD_TOP}/${device_fde}/FdeVncClient.apk
+ systemui_download_url=`cat ${ANDROID_BUILD_TOP}/vendor/prebuilts/bdapps/bin/BoringdroidSystemUI`
+ wget ${systemui_download_url} -O ${ANDROID_BUILD_TOP}/vendor/prebuilts/bdapps/bin/BoringdroidSystemUI.apk
 }
