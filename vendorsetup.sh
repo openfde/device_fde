025eadb358e63d165b078a5ffcd884aad45a15cc
gongyong <185457686@qq.com>
2024-01-15 11:15:04 +0800

    update vendorsetup function and add md5sum of apks into config

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,10 +1,29 @@
 
-device_fde="device/openfde/fde/fde_vnc"
+
 function syncFdeApk {
- vnc_download_url=`cat ${ANDROID_BUILD_TOP}/${device_fde}/FdeVncClient`
- wget ${vnc_download_url} -O ${ANDROID_BUILD_TOP}/${device_fde}/FdeVncClient.apk
- systemui_download_url=`cat ${ANDROID_BUILD_TOP}/vendor/prebuilts/bdapps/bin/BoringdroidSystemUI`
- wget ${systemui_download_url} -O ${ANDROID_BUILD_TOP}/vendor/prebuilts/bdapps/bin/BoringdroidSystemUI.apk
- gallery_download_url=`cat ${ANDROID_BUILD_TOP}/device/openfde/fde/fde_gallery/FdeGallery`
- wget ${gallery_download_url} -O ${ANDROID_BUILD_TOP}/device/openfde/fde/fde_gallery/FdeGallery.apk
+	configPath=${ANDROID_BUILD_TOP}/device/openfde/fde/fde_vnc/FdeVncClient 
+	compareAndDownload  ${configPath}  ${configPath}.apk 
+
+	configPath=${ANDROID_BUILD_TOP}/vendor/prebuilts/bdapps/bin/BoringdroidSystemUI
+	compareAndDownload  ${configPath}  ${configPath}.apk 
+
+	configPath=${ANDROID_BUILD_TOP}/device/openfde/fde/fde_gallery/FdeGallery
+	compareAndDownload  ${configPath}  ${configPath}.apk 
 }
+
+function compareAndDownload  {
+	download_url=`cat ${1} |awk  '{print $1}'`
+	md5=`cat ${1} |awk '{print $2}'`
+	noNeedDownload=0
+	if [  -n "$md5" ];then
+		if [ -e ${2} ];then
+			currentMd5=`md5sum ${2} |awk  '{print $1}'`
+				if [ "$currentMd5" = "$md5" ];then
+					noNeedDownload=1
+				fi	
+		fi
+	fi
+	if [ $noNeedDownload -eq 0 ];then
+		wget ${download_url} -O ${2}
+	fi
+}	
