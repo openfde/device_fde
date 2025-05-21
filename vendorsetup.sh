10c232eecde68716c8d003897613381de32bf13e
gongyong <185457686@qq.com>
2025-05-21 14:14:23 +0800

    add config for boringsystemui

---
+++ vendorsetup.sh
@@ -0,0 +1,23 @@
+
+
+function syncFdeApk {
+ 	configPath=${ANDROID_BUILD_TOP}/device/openfde/fde/fde_systemui/BoringdroidSystemUI
+	compareAndDownload  ${configPath}  ${configPath}.apk
+}
+
+function compareAndDownload  {
+	download_url=`cat ${1} |awk  '{print $1}'`
+	md5=`cat ${1} |awk '{print $2}'`
+	noNeedDownload=0
+	if [  -n "$md5" ];then
+		if [ -e ${2} ];then
+			currentMd5=`md5sum ${2} |awk  '{print $1}'`
+				if [ "$currentMd5" = "$md5" ];then
+					noNeedDownload=1
+				fi	
+		fi
+	fi
+	if [ $noNeedDownload -eq 0 ];then
+		wget ${download_url} -O ${2}
+	fi
+}	
