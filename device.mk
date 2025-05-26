9a1d0d0d6b6677f15ce91e24532dca82c0f2f5ad
tb <ali609386314@gmail.com>
2025-05-26 16:20:29 +0800

    use multi_windows

--- device.mk
+++ device.mk
@@ -180,9 +180,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.vulkan.level-1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.level.xml \
     frameworks/native/data/etc/android.hardware.vulkan.version-1_1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.version.xml \
     frameworks/native/data/etc/android.hardware.vulkan.compute-0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.compute.xml \
-    frameworks/native/data/etc/android.software.freeform_window_management.xml:system/etc/permissions/android.software.freeform_window_management.xml \
     frameworks/base/data/etc/com.android.oobe.xml:system/system_ext/etc/permissions/com.android.oobe.xml
 
+#frameworks/native/data/etc/android.software.freeform_window_management.xml:system/etc/permissions/android.software.freeform_window_management.xml
 
 # magic window
 PRODUCT_COPY_FILES += \
