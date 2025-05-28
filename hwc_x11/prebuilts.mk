8a04ce7f0a55c1647b6ae86a229a475593187195
huyang <1184394624@qq.com>
2025-05-28 12:02:28 +0800

    hwc_x11: add x11 lib

---
+++ hwc_x11/prebuilts.mk
@@ -0,0 +1,6 @@
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/x11/libX11.so:/system/lib64/libX11.so \
+	$(LOCAL_PATH)/x11/libXau.so:/system/lib64/libXau.so \
+	$(LOCAL_PATH)/x11/libxcb.so:/system/lib64/libxcb.so \
+	$(LOCAL_PATH)/x11/libXcomposite.so:/system/lib64/libXcomposite.so \
+	$(LOCAL_PATH)/x11/libXdmcp.so:/system/lib64/libXdmcp.so
