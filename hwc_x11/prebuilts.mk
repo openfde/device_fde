6c8ada108a8c2118764269542c6964319549463b
huyang <1184394624@qq.com>
2025-05-28 15:15:59 +0800

    hwc_x11: add x11 lib - 5

--- hwc_x11/prebuilts.mk
+++ hwc_x11/prebuilts.mk
@@ -1,9 +1,10 @@
+ifeq ($(TARGET_PRODUCT),$(filter $(TARGET_PRODUCT),openfde_fde_arm64 openfde_fde_arm64_only openfde_x100_arm64))
+
+
 HWCX11_PRODUCT_PACKAGES := \
-    libX11 \
-    libXau \
-    libxcb \
-    libXcomposite \
-    libXdmcp
+    libX11
 
 PRODUCT_PACKAGES += \
         $(HWCX11_PRODUCT_PACKAGES)
+
+endif
