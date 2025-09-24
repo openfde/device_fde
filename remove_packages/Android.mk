cb1df06c221d6c33401bb1688d11a62b9690f395
xudingqiang <xudingqiang520@126.com>
2025-09-24 11:09:53 +0800

    remove Gallery2

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := AudioFX Bluetooth BluetoothMidiService Contacts Jelly Eleven Updater Glimpse
+LOCAL_OVERRIDES_PACKAGES := AudioFX Bluetooth BluetoothMidiService Contacts Jelly Eleven Updater Glimpse Gallery2
 #LineageSetupWizard
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
